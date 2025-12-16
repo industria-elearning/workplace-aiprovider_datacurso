3253d1f729219f8b76ccee545174abc86c4a2a45
PieroLS04 <piero@datacurso.com>
2025-12-16 09:13:49 -0500

    feat: table config plugins

--- db/upgrade.php
+++ db/upgrade.php
@@ -131,5 +131,31 @@ function xmldb_aiprovider_datacurso_upgrade($oldversion) {
         // Datacurso savepoint reached.
         upgrade_plugin_savepoint(true, 2025120301, 'aiprovider', 'datacurso');
     }
+
+    if ($oldversion < 2025120303) {
+
+        // Define table config_plugins_datacurso to be created.
+        $table = new xmldb_table('config_plugins_datacurso');
+
+        // Adding fields to table config_plugins_datacurso.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('plugin', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+        $table->add_field('tenant_id', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+        $table->add_field('value', XMLDB_TYPE_TEXT, null, null, null, null, null);
+
+        // Adding keys to table config_plugins_datacurso.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('plugin_tenant_name_uk', XMLDB_KEY_UNIQUE, ['plugin', 'tenant_id', 'name']);
+
+        // Conditionally launch create table for config_plugins_datacurso.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Datacurso savepoint reached.
+        upgrade_plugin_savepoint(true, 2025120303, 'aiprovider', 'datacurso');
+    }
+
     return true;
 }
