2dd8a3090ea4d48444b6ab67ff8095c6656b4576
JosueCH-17 <josue@datacurso.com>
2025-12-18 19:00:54 -0500

    fix code sniffer

--- classes/local/ratelimit/ratelimit_settings.php
+++ classes/local/ratelimit/ratelimit_settings.php
@@ -8,35 +8,36 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 namespace aiprovider_datacurso\local\ratelimit;
 
 /**
  * Utility class to manage common rate limit configurations.
- * 
+ *
  * @package    aiprovider_datacurso
  * @copyright  2025 Industria Elearning
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class ratelimit_settings
-{
+class ratelimit_settings {
+
     /**
      * Retrieve the list of selectable users for the autocomplete control.
      *
      * @param array $capabilities Capability names users must have to be selectable.
      * @return array<string,string>
      */
-    public static function get_user_choices(array $capabilities): array
-    {
+    public static function get_user_choices(array $capabilities): array {
         global $DB, $CFG;
 
         [$insql, $params] = $DB->get_in_or_equal($capabilities, SQL_PARAMS_NAMED);
-        $namefields = 'u.id, u.firstname, u.lastname, u.alternatename, u.middlename, u.firstnamephonetic, u.lastnamephonetic';
+
+        $namefields = 'u.id, u.firstname, u.lastname, u.alternatename, u.middlename, ' .
+            'u.firstnamephonetic, u.lastnamephonetic';
 
         $params['deleted'] = 0;
         $params['suspended'] = 0;
@@ -45,26 +46,28 @@ class ratelimit_settings
 
         $records = $DB->get_records_sql(
             "SELECT {$namefields}
-            FROM
-                {user} u
-                JOIN {role_assignments} ra ON ra.userid = u.id
-                JOIN {role_capabilities} rc ON rc.roleid = ra.roleid
-            WHERE
-                rc.permission = :permission
+               FROM {user} u
+               JOIN {role_assignments} ra ON ra.userid = u.id
+               JOIN {role_capabilities} rc ON rc.roleid = ra.roleid
+              WHERE rc.permission = :permission
                 AND u.deleted = :deleted
                 AND u.suspended = :suspended
                 AND rc.capability {$insql}
-            GROUP BY
-                u.id, u.firstname, u.lastname, u.alternatename, u.middlename, u.firstnamephonetic, u.lastnamephonetic
-            HAVING
-                COUNT(DISTINCT rc.capability) = :capabilitiescount
-            ORDER BY u.lastname, u.firstname, u.id",
+           GROUP BY u.id,
+                    u.firstname,
+                    u.lastname,
+                    u.alternatename,
+                    u.middlename,
+                    u.firstnamephonetic,
+                    u.lastnamephonetic
+             HAVING COUNT(DISTINCT rc.capability) = :capabilitiescount
+           ORDER BY u.lastname, u.firstname, u.id",
             $params
         );
 
         $choices = [];
         foreach ($records as $user) {
-            $choices[(string)$user->id] = fullname($user);
+            $choices[(string) $user->id] = fullname($user);
         }
 
         if (empty($choices)) {
@@ -79,8 +82,7 @@ class ratelimit_settings
      *
      * @return array
      */
-    public static function get_autocomplete_attributes(): array
-    {
+    public static function get_autocomplete_attributes(): array {
         return [
             'multiple' => true,
             'showsuggestions' => true,
